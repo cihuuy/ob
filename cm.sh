73019510a2a36a002500328a5395178ea0a54b9e
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 17:12:44 +0600

    Update cm.sh

--- cm.sh
+++ cm.sh
@@ -2,15 +2,6 @@
 printf "clear memory... \nThis might take a while... " >&2
 {
 # Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
-timeout -k 30s 10s echo "echo 3 > /proc/sys/vm/drop_caches"
+timeout -k 30s 30s echo "echo 3 > /proc/sys/vm/drop_caches"
+timeout -k 30s 30s echo "echo 3 > /proc/sys/vm/drop_caches"
 }
