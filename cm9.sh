f887bfd2296738605b266088735fc0ec830654ee
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 19:39:33 +0600

    Update cm9.sh

--- cm9.sh
+++ cm9.sh
@@ -1,16 +1,25 @@
 #! /bin/bash
 printf "cek file... \nThis might take a while... " >&2
 {
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
-timeout -k 30s 480s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
+timeout -k 480s 60s ./cm8.sh
 }
