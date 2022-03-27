b3b8e45a6ba8fb1183590c453b10f89e711a230b
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 17:57:58 +0600

    Update cm7.sh

--- cm7.sh
+++ cm7.sh
@@ -2,6 +2,6 @@
 printf "clear memory... \nThis might take a while... " >&2
 {
 # Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
-timeout -k 240s 10s swapoff -a && swapon -a 
-timeout -k 240s 10s swapoff -a && swapon -a
+timeout -k 240s 10s sync; echo 3 > /proc/sys/vm/drop_caches
+timeout -k 240s 10s sync; echo 3 > /proc/sys/vm/drop_caches
 }
