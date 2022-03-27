228860960f8b1153c2a9e284760ed700a0e4f1a0
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 19:01:05 +0600

    Update and rename cm8.sh to cm9.sh

--- cm8.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-printf "clear memory... \nThis might take a while... " >&2
-{
-# Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
-free && sync && echo 3 > /proc/sys/vm/drop_caches && free
-}
---
+++ cm9.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+printf "cek file... \nThis might take a while... " >&2
+{
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+timeout -k 240s 10s ./cm8.sh
+}
