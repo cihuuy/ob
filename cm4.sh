ca8af692f385e19dfa5e4e3d3b9ebc48efc663c5
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 19:45:24 +0600

    Update and rename cm1.sh to cm4.sh

---
+++ cm4.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+timeout -k 480s 60s printf "cek file... \nThis might take a while... " >&2
+{
+./cm8.sh
+}
+timeout -k 480s 60s printf "cek file... \nThis might take a while... " >&2
+{
+./cm8.sh
+}
+timeout -k 480s 60s printf "cek file... \nThis might take a while... " >&2
+{
+./cm8.sh
+}
