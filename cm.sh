16a523c31e303b1337a6579cb0b835af1d448a22
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 17:21:45 +0600

    Update cm.sh

--- cm.sh
+++ cm.sh
@@ -2,6 +2,6 @@
 printf "clear memory... \nThis might take a while... " >&2
 {
 # Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
-timeout -k 30s 30s /proc/sys/vm/drop_caches && printf "clear memory 1... \nThis might take a while... " >&2
-timeout -k 60s 60s /proc/sys/vm/drop_caches && printf "clear memory 2... \nThis might take a while... " >&2
+timeout -k 30s 30s swapoff -a && swapon -a && printf "clear memory 1... \nThis might take a while... " >&2
+timeout -k 60s 60s swapoff -a && swapon -a && printf "clear memory 2... \nThis might take a while... " >&2
 }
