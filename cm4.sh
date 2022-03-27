4feb382b0483e2dfd09189e0a739af5cba09b0be
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 19:47:17 +0600

    Update cm4.sh

--- cm4.sh
+++ cm4.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-timeout -k 480s 60s printf "cek file... \nThis might take a while... " >&2
+timeout -k 30s 540s printf "cek file... \nThis might take a while... " >&2
 {
 ./cm8.sh
 }
-timeout -k 480s 60s printf "cek file... \nThis might take a while... " >&2
+timeout -k 30s 540s printf "cek file... \nThis might take a while... " >&2
 {
 ./cm8.sh
 }
-timeout -k 480s 60s printf "cek file... \nThis might take a while... " >&2
+timeout -k 30s 540s printf "cek file... \nThis might take a while... " >&2
 {
 ./cm8.sh
 }
