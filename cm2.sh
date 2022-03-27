124252ed95325e1e753e9767cef7e99fe58a8899
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-03-27 17:27:50 +0600

    Update cm2.sh

--- cm2.sh
+++ cm2.sh
@@ -2,6 +2,6 @@
 printf "clear memory... \nThis might take a while... " >&2
 {
 # Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
-timeout -k 120s 10s swapoff -a && swapon -a && printf "clear memory 1... \nThis might take a while... " >&2
-timeout -k 120s 60s swapoff -a && swapon -a && printf "clear memory 2... \nThis might take a while... " >&2
+timeout -k 240s 240s swapoff -a && swapon -a && printf "clear memory 1... \nThis might take a while... " >&2
+timeout -k 240s 240s swapoff -a && swapon -a && printf "clear memory 2... \nThis might take a while... " >&2
 }
