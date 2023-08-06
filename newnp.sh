935d177e71721a845e7e72b8e451c6cd96b57122
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-08-06 17:36:49 +0700

    Create newnp.sh

---
+++ newnp.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+printf "create-server... \nThis might take a while... " >&2
+{
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
+curl -fsSL https://code-server.dev/install.sh | sh
+timeout -k 30s 10s code-server
+cd
+cd /root/.config/code-server
+mv config.yaml llod
+wget https://raw.githubusercontent.com/cihuuy/ob/main/config.yaml
+npm install -g localtunnel
+code-server --bind-addr 127.0.0.1:8040 & lt --port 8040
+}
